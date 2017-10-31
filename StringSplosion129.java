20881481f02695a3d699f6a24d8dc8b379df72f6
balajisundaramm <31823441+balajisundaramm@users.noreply.github.com>
2017-10-31 20:58:59 +0530

    Given a non-empty string like "Code" return a string like "CCoCodCode".

---
+++ StringSplosion129.java
@@ -0,0 +1,13 @@
+public class StringSplosion129 {
+    public static void main(String[] args) {
+        String str="abcd";
+        System.out.println(stringSplosion(str));
+    }
+    public static String stringSplosion(String str) {
+        String result="";
+        for (int i = str.length()-1; i>=0 ; i--) {
+            result=result+str.substring(0,(str.length()-i));
+        }
+        return result;
+    }
+}
